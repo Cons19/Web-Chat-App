fff904b7463bf5a503b4216065904d5921b700dd
MariusMM2 <dmunteanu91@yahoo.com>
2018-12-09 13:24:34 +0100

    simplified code

--- index.js
+++ index.js
@@ -2,35 +2,18 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-users = [];
-connections = [];
-
-
 app.get('/', function(req, res){
- 	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-	console.log('someone connected');
-
-	connections.push(socket);
-	console.log('Connected: %s sockets connected ', connections.length);
-	
-    socket.username = "Anonymous";
-	
-	//send message
-    socket.on('chat message', function(message){
-        
+	console.log('someone connected!');
+	socket.on('chat message', function(message) {
 		console.log(message);
-        
-        
-		io.emit('chat message', socket.username + ':' + message);
+		io.emit('chat message', message); 
 	});
 });
 
-http.listen(8080, function(){
-    
-	console.log('listening on port 8080');
-
+http.listen(8003, function(){
+	console.log('listening on port 8003');
 });
-
